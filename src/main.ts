86d6b65e00a60c87ad99f7de5123b4de5075941f
Aymar <gardinaymar35@gmail.com>
2026-09-20 01:03:45 -0500

    feat: agregar helmet para cabeceras de seguridad

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 ﻿import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
